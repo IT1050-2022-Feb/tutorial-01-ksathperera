03741f97750d8e974ac647c5b92e9519879a8738
Kavindi Perera <90544783+ksathperera@users.noreply.github.com>
2022-03-09 19:58:05 +0000

    I have completed tutorial 2

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,25 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+
+  int noOfkm;
+  float amount;
+
+  printf("Enter the number of kilo meters travelled: ");
+  scanf("%d",&noOfkm);
+
+  puts("");
+
+  if(noOfkm<=30)
+  {
+    amount = noOfkm * 50;
+  }
+  else
+  {
+    amount = 30*50 + (noOfkm - 30)*40;
+    
+  }
+  printf("Total amount to be paid for rented vehicle : %.2f",amount);
   
   return 0;
 }
